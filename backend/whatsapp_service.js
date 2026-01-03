52faf12b851226c0ae91ab4ae6300b482d44d5a1
oziqx <oguzhan.lion44@gmail.com>
2026-01-03 15:59:03 +0300

    feat: add WhatsApp service for sending messages and managing client status

--- backend/whatsapp_service.js
+++ backend/whatsapp_service.js
@@ -13,7 +13,13 @@ function initWhatsApp() {
         }),
         puppeteer: {
             headless: true,
-            args: ['--no-sandbox', '--disable-setuid-sandbox']
+            executablePath: '/usr/bin/google-chrome-stable',
+            args: [
+                '--no-sandbox',
+                '--disable-setuid-sandbox',
+                '--disable-dev-shm-usage',
+                '--disable-gpu'
+            ]
         }
     });
 
